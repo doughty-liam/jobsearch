68ae9daf265c87844d803eb78cebc864a9f20aa7
doughty-liam <liamdoughty5@gmail.com>
2024-06-27 11:43:04 -0400

    Added offset pagination to results. Need to fix visuals and add next/last page buttons.

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -11,6 +11,10 @@
 						v-on:click="updateJobDetails(job.id, job.title, job.companyName, job.location, job.description)">
 					</jobPosting>
 				</ul>
+				<div id="resultsNav">
+					<button @click="nextPage"></button>
+<!-- 					<input type="number" max="10" id="pageNumInput" :value="offset+1" :onChange="updateResults(2)">
+ -->			</div>
 			</div>
 			<div id="details">
 				<jobDetails :key="selectedJob.id" :title="selectedJob.title" :companyName="selectedJob.company"
@@ -26,33 +30,74 @@ import jobPosting from './components/jobPosting.vue'
 import sortOptions from './components/sortOptions.vue'
 import jobDetails from './components/jobDetails.vue'
 import sortMenu from './components/sortMenu.vue'
-import { computed } from 'vue'
+import { ref, watch } from 'vue'
 import gql from 'graphql-tag'
 import { provideApolloClient, useQuery } from '@vue/apollo-composable'
 import client from './apollo-config'
 
+
 export default {
-	name: 'App',
 	components: {
 		jobPosting,
 		sortOptions,
-		sortMenu,
-		jobDetails
+		jobDetails,
+		sortMenu
 	},
-	data() {
 
-		return {
-			selectedJob: {},
-			menuActive: false,
+	setup() {
+		const jobsPerPage = ref(10)
+		const offset = ref(0)
+		const jobs = ref([])
+		const selectedJob = ref({})
+
+
+		provideApolloClient(client)
+		const { result, fetchMore } = useQuery(gql`
+			query getRankedJobs($first: Int, $skip: Int) {
+				jobsBySimilarity(first: $first, skip: $skip) {
+					id
+					title
+					companyName
+					location
+					description
+					applied
+				}
+			}`,
+			{
+				first: jobsPerPage.value,
+				skip: offset.value
+			}
+		)
+
+		watch(result, (newResult) => {
+			if (newResult && newResult.jobsBySimilarity) {
+				jobs.value = newResult.jobsBySimilarity
+				console.log(jobs.value)
+			}
+		})
+
+		const nextPage = async () => {	
+			console.log("fetchJobs called.")
+			offset.value += jobsPerPage.value
+			fetchMore({
+				variables: {
+					skip: offset.value
+				},
+				updateQuery: (result, {fetchMoreResult}) => {
+					if(!fetchMoreResult) return result
+					
+					jobs.value = fetchMoreResult.jobsBySimilarity
+					console.log(jobs.value)
+
+				}
+			})
+
 		}
-	},
 
-	methods: {
-		updateJobDetails(id, title, companyName, location, description) {
-
+		const updateJobDetails = (id, title, companyName, location, description) => {
 			companyName = companyName + " | ";
 
-			this.selectedJob = {
+			selectedJob.value = {
 				id: id,
 				title: title,
 				company: companyName,
@@ -62,51 +107,20 @@ export default {
 
 			const page_title = document.getElementById("page_title");
 			const detailsContainer = document.getElementById("details");
-			const postingViewed = document.getElementById(id);
 
 			page_title.innerHTML = title;
 			detailsContainer.style.visibility = "visible";
-			postingViewed.style.backgroundColor = "#234A74"; // This not working :(
-
-		},
-		toggle() {
-
-			/* handle event emitted by filter button press in
-			sortOptions */
-
 		}
-	},
 
-	setup() {
-
-		provideApolloClient(client);
-
-		/*
-			Add in logic to display N number of jobs per page
-			skip N*currPageNum
-		
-		const jobsPerPage
-		const currPageNum
-		*/
-		const { result } = useQuery(gql`
-			query {
-				jobsBySimilarity(first:10, skip:0) {
-				id
-				title
-				companyName
-				location
-				description
-				applied
-			}
-			}
-			`,
-		);
-
-		const jobs = computed(() => result.value?.jobsBySimilarity ?? []);
 		return {
 			jobs,
+			offset,
+			jobsPerPage,
+			nextPage,
+			updateJobDetails,
+			selectedJob
 		}
-	},
+	}
 }
 
 </script>
@@ -150,6 +164,32 @@ export default {
 	height: calc(100vh - 65px);
 	width: 100%;
 	overflow-y: scroll;
+	background-color: rgb(210, 124, 124);
+}
+
+#resultsNav {
+	background-color: yellow;
+	height: 50px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+input::-webkit-inner-spin-button,
+input::-webkit-outer-spin-button {
+	-webkit-appearance: none;
+	margin: 0;
+}
+
+#pageNumInput {
+	margin: 0 auto;
+	width: 25px;
+	height: 60%;
+	text-align: center;
+	font-size: 16pt;
+	-webkit-appearance: none;
+	appearance: none;
 }
 
 ::-webkit-scrollbar {
